3d79d79828f32812326ba42549e0cf61996530e2
Patrick Desjardins <mrdesjardins@gmail.com>
2016-06-10 14:19:43 -0700

    Create an interface to abstract two different ways to get the data from the backend.

--- scripts/app.ts
+++ scripts/app.ts
@@ -9,80 +9,74 @@ import VSS_WebApi = require("VSS/WebApi/RestClient");
 
 import {msengToken} from "scripts/secret"
 
-getBoards();
-getMsEngBoards().then((value:Work_Contracts.BoardReference[]) => {
+var dataProvider = new DataProvider();
+dataProvider.getBoards().then((value:Work_Contracts.BoardReference[]) => {
     console.log(value);
 }, (err)=>{console.log(err)});
 
+export interface IDataProvider{
+     getBoard(board: string): IPromise<Work_Contracts.Board>;
+     getBoards(): IPromise<Work_Contracts.BoardReference[]>;
+}
+export class DataProvider implements IDataProvider{
 
-function getTeamContext(): TFS_Core_Contracts.TeamContext {
-    var context = VSS.getWebContext();
-    return {
-        projectId: context.project.id,
-        project: context.project.name,
-        teamId: context.team.id,
-        team: context.team.name
+    private getTeamContext(): TFS_Core_Contracts.TeamContext {
+        var context = VSS.getWebContext();
+        return {
+            projectId: context.project.id,
+            project: context.project.name,
+            teamId: context.team.id,
+            team: context.team.name
+        }
+    }
+
+    public getBoard(board: string): IPromise<Work_Contracts.Board> {
+        var restClient = RestClient.getClient();
+        return restClient.getBoard(this.getTeamContext(), board);
+    }
+
+    public getBoards(): IPromise<Work_Contracts.BoardReference[]> {
+        var restClient = RestClient.getClient();
+        return restClient.getBoards(this.getTeamContext());        
     }
 }
 
-function getBoard(board: string) {
-    var restClient = RestClient.getClient();
-    restClient.getBoard(getTeamContext(), board).then((board: Work_Contracts.Board) => {
-        // do something
-        board.allowedMappings;
-    });
-}
+export class DevDataProvider implements IDataProvider{
 
-function getBoards() {
-    var restClient = RestClient.getClient();
-    restClient.getBoards(getTeamContext()).then((boardRefs: Work_Contracts.BoardReference[]) => {
-        getBoard(boardRefs[0].name);
-    });
-}
 
-/*
-$( document ).ready(function() {
-        $.ajax({
-            url: 'https://mseng.visualstudio.com/defaultcollection/_apis/projects?api-version=1.0',
-            dataType: 'json',
-            headers: {
-                'Authorization': 'Basic ' + btoa(":"+msengToken)
-            }
-        }).done(function( results ) {
-            console.log( results.value[0].id + " " + results.value[0].name );
+    public getMsEngTeamContext(): TFS_Core_Contracts.TeamContext {
+        return {
+            projectId: "",
+            project: "VSOnline",
+            teamId: "",
+            team: "Blueprint"
+        }
+    }
+
+    public getBoards(): IPromise<Work_Contracts.BoardReference[]>
+    {
+        var teamContext = this.getMsEngTeamContext();
+        var project = teamContext.projectId || teamContext.project;
+        var team = teamContext.teamId || teamContext.team;
+        var webApi = new VSS_WebApi.VssHttpClient("https://mseng.visualstudio.com/");
+
+        return webApi._beginRequest<Work_Contracts.BoardReference[]>({
+            httpMethod: "GET",
+            area:  "work",
+            locationId: "23ad19fc-3b8e-4877-8462-b3f92bc06b40",
+            resource: "boards",
+            routeTemplate: "{project}/{team}/_apis/{area}/{resource}/{id}",
+            responseIsCollection: true,
+            routeValues: {
+                project: project,
+                team: team,
+            },
+            apiVersion: "2.0-preview.1",
+            customHeaders : {'Authorization': 'Basic ' + btoa(":"+msengToken)}
         });
-    });
-*/
-
-
-function getMsEngTeamContext(): TFS_Core_Contracts.TeamContext {
-    return {
-        projectId: "",
-        project: "VSOnline",
-        teamId: "",
-        team: "Blueprint"
     }
-}
 
-function getMsEngBoards(): IPromise<Work_Contracts.BoardReference[]>
-{
-    var teamContext = getMsEngTeamContext();
-    var project = teamContext.projectId || teamContext.project;
-    var team = teamContext.teamId || teamContext.team;
-    var webApi = new VSS_WebApi.VssHttpClient("https://mseng.visualstudio.com/");
-
-    return webApi._beginRequest<Work_Contracts.BoardReference[]>({
-        httpMethod: "GET",
-        area:  "work",
-        locationId: "23ad19fc-3b8e-4877-8462-b3f92bc06b40",
-        resource: "boards",
-        routeTemplate: "{project}/{team}/_apis/{area}/{resource}/{id}",
-        responseIsCollection: true,
-        routeValues: {
-            project: project,
-            team: team,
-        },
-        apiVersion: "2.0-preview.1",
-        customHeaders : {'Authorization': 'Basic ' + btoa(":"+msengToken)}
-    });
+    public getBoard(board: string): IPromise<Work_Contracts.Board> {
+        return null;
+    }
 }
