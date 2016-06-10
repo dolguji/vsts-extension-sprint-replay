e1326bd192e0dfe2983c7f25c460dc19c9d70e7c
Jessica <jechayav@microsoft.com>
2016-06-10 14:42:38 -0700

    Add queryByWiql

--- scripts/app.ts
+++ scripts/app.ts
@@ -9,6 +9,8 @@ import VSS_WebApi = require("VSS/WebApi/RestClient");
 
 import {msengToken} from "scripts/secret"
 
+export var defaultFields = ["System.Id","System.WorkItemType","System.Title","System.State","System.AssignedTo"];
+
 var dataProvider = new DataProvider();
 dataProvider.getBoards().then((value:Work_Contracts.BoardReference[]) => {
     console.log(value);
@@ -20,15 +22,42 @@ export interface IDataProvider{
 }
 export class DataProvider implements IDataProvider{
 
-    private getTeamContext(): TFS_Core_Contracts.TeamContext {
-        var context = VSS.getWebContext();
-        return {
-            projectId: context.project.id,
-            project: context.project.name,
-            teamId: context.team.id,
-            team: context.team.name
+    public queryByWiql(teamContext: TFS_Core_Contracts.TeamContext, workItemTypes: string[], columnNames: string[], date: string): IPromise<TFS_Wit_Contracts.WorkItemQueryResult> {
+        var queryBuilder = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.State] FROM WorkItems WHERE [System.TeamProject] = '" + teamContext.project + "'";
+        
+        for (var i=0; i < workItemTypes.length; i++) {
+            if (i == 0) {
+                queryBuilder += " AND ([System.WorkItemType] = '" + workItemTypes[i] + "'";
+            }
+            else {
+                queryBuilder += " OR [System.WorkItemType] = '" + workItemTypes[i] + "'";
+            }
         }
+        
+        for (var i=0; i < columnNames.length; i++) {
+            if (i == 0) {
+                queryBuilder += " AND [System.BoardColumn] = '" + columnNames[i] + "'";
+            }
+            else {
+                queryBuilder += " OR [System.BoardColumn] = '" + columnNames[i] + "'";
+            }
+        }
+        queryBuilder += ")";
+        
+        queryBuilder += " AsOf '" + date + "' ORDER BY [System.ChangedDate] DESC";
+        
+        var wiql = {
+            query: queryBuilder
+        }    
+
+        var witClient = TFS_Wit_Client.getClient();
+        return witClient.queryByWiql(wiql, teamContext.projectId, teamContext.teamId);
     }
+    
+    public getWorkItems(workItemIds: number[], asOf?: Date): IPromise<TFS_Wit_Contracts.WorkItem[]> {
+        var witClient = TFS_Wit_Client.getClient();
+        return witClient.getWorkItems(workItemIds, defaultFields, asOf);
+    }   
 
     public getBoard(board: string): IPromise<Work_Contracts.Board> {
         var restClient = RestClient.getClient();
@@ -38,6 +67,27 @@ export class DataProvider implements IDataProvider{
     public getBoards(): IPromise<Work_Contracts.BoardReference[]> {
         var restClient = RestClient.getClient();
         return restClient.getBoards(this.getTeamContext());        
+    }
+    
+    private getTeamContext(): TFS_Core_Contracts.TeamContext {
+        var context = VSS.getWebContext();
+        return {
+            projectId: context.project.id,
+            project: context.project.name,
+            teamId: context.team.id,
+            team: context.team.name
+        }
+    }
+    
+    private getInProgressColumnNames(boardColumns: Work_Contracts.BoardColumn[]): string[] {
+        var columnNames = [];
+        for (var i=0; i < boardColumns.length; i++) {
+            var type = boardColumns[i].columnType;
+            if (type == Work_Contracts.BoardColumnType.InProgress) {
+                columnNames.push(boardColumns[i].name);
+            }
+        }
+        return columnNames;
     }
 }
 
