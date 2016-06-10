cd93a97c69d86d86895e8663b4e6a45092c9a518
Jessica <jechayav@microsoft.com>
2016-06-10 12:00:58 -0700

    Add getBoard function

--- scripts/app.ts
+++ scripts/app.ts
@@ -6,26 +6,27 @@ import RestClient = require("TFS/Work/RestClient");
 import Work_Contracts = require("TFS/Work/Contracts");
 import TFS_Core_Contracts = require("TFS/Core/Contracts");
 
-
-function getBoardColumns(teamContext: TFS_Core_Contracts.TeamContext, board: string) {
-    var restClient = RestClient.getClient();
-    restClient.getBoardColumns(teamContext, board).then((boardRefs: Work_Contracts.BoardColumn[]) => {
-        // do something
-    });
-}
-
-function getBoards() {
-    var restClient = RestClient.getClient();
+function getTeamContext(): TFS_Core_Contracts.TeamContext {
     var context = VSS.getWebContext();
-    
-    var teamContext: TFS_Core_Contracts.TeamContext = {
+    return {
         projectId: context.project.id,
         project: context.project.name,
         teamId: context.team.id,
         team: context.team.name
     }
-    
-    restClient.getBoards(teamContext).then((boardRefs: Work_Contracts.BoardReference[]) => {
+}
+
+function getBoard(board: string) {
+    var restClient = RestClient.getClient();
+    restClient.getBoard(getTeamContext(), board).then((board: Work_Contracts.Board) => {
+        // do something
+        board.allowedMappings;
+    });
+}
+
+function getBoards() {
+    var restClient = RestClient.getClient();
+    restClient.getBoards(getTeamContext()).then((boardRefs: Work_Contracts.BoardReference[]) => {
         alert("First boardReference " + boardRefs[0].name);
     });
 }
@@ -36,7 +37,7 @@ $( document ).ready(function() {
             url: 'https://mseng.visualstudio.com/defaultcollection/_apis/projects?api-version=1.0',
             dataType: 'json',
             headers: {
-                'Authorization': 'Basic ' + btoa(":YOURTOKENHERE")
+                'Authorization': 'Basic ' + btoa(":v7dqy6m7ebz27nw6sk77qwtuksqu2dclgtwmbulpqgstaiz2edxa")
             }
         }).done(function( results ) {
             console.log( results.value[0].id + " " + results.value[0].name );
