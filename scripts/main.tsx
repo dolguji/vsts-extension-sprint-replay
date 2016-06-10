c39b4bbca8ef210e203008e4684064c820ab4a1e
Hyung Kwon <hykwon@microsoft.com>
2016-06-10 12:12:49 -0700

    adding board column component

--- scripts/main.tsx
+++ scripts/main.tsx
@@ -2,23 +2,57 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 interface IBoardComponenetState {
-    name: string
+    name: string;
+    columns: string[];
 }
 
 export class BoardComponent extends React.Component<any, IBoardComponenetState> {
 	constructor() {
         super();
         this.state = {
-            name: "hyung"
+            name: "hyung 2",
+            columns: []
         };
     }
 	
+    componentDidMount() {
+        this.setBoardConfiguration({ name: "User Story", columns: ["New", "Active", "Resolved", "Closed"]});
+    }
+    
+    public setBoardConfiguration(boardState: IBoardComponenetState ){
+        this.setState({
+            name: boardState.name,
+            columns: boardState.columns
+        });
+    } 
+    
 	public render() {
+        
+        
+        
 		return <div> My name is {this.state.name}</div>;
 	}
 }
 
+export class BoardColumn extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+        };
+    }
+    
+    public render() {
+		return <div> Column name: {this.state.name}</div>;
+	}
+}
+
+let data ={
+    
+};
+
 let element = document.getElementById("sprint-replay-container");
-let boardComponent: BoardComponent;
-ReactDOM.render(<BoardComponent ref={(i) => boardComponent = i} />, element);
+ReactDOM.render(<BoardComponent data={data} />, element);
+
 
